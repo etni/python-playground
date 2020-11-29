ad0c3c0f2ba5398533eeab2a73b29457006929d3
Etni Alvis <etni.alvis@gmail.com>
2020-11-29 09:51:57 -0500

    string manipulation

---
+++ strings.py
@@ -0,0 +1,11 @@
+# manipulating strings
+
+string1 = "0102030405060708090A"
+string2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+print(string1)
+
+print(string1[-1])      # last char
+print(string1[0:2])     # first two chars,
+print(string1[:2])      #
+print(string1[1::2])    # 3rd parameter skips chars
